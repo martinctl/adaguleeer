8c5d4c3980195cafac97d62f7e2dcd01bfda581e
Martin Catheland <m.catheland@gmail.com>
2024-12-20 07:57:32 +0100

    fix: change bar-race chart

--- website/components/subs/bar-race.tsx
+++ website/components/subs/bar-race.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as d3 from 'd3'
+import { PlayIcon, PauseIcon, ResetIcon } from '@radix-ui/react-icons'
 
 interface BarRaceProps {
   data: {
@@ -24,6 +25,9 @@ interface DataItem {
 
 export default function BarRace({ data, width = 800, height = 500 }: BarRaceProps) {
   const svgRef = useRef<SVGSVGElement>(null)
+  const [isPlaying, setIsPlaying] = useState(true)
+  const [currentFrame, setCurrentFrame] = useState(0)
+  const animationRef = useRef<number>(0)
 
   useEffect(() => {
     if (!svgRef.current) return
@@ -53,7 +57,7 @@ export default function BarRace({ data, width = 800, height = 500 }: BarRaceProp
         if (x < 1000000) {
             return `${x}`
         }
-        return `${(x / 1000000).toFixed(1)}M`
+        return `${(x / 1000000)}M`
     }
     const formatDate = d3.utcFormat("%d %b %Y")
 
@@ -121,9 +125,25 @@ export default function BarRace({ data, width = 800, height = 500 }: BarRaceProp
       .attr('text-anchor', 'end')
 
     // Implementation of the axis
-    const axis = svg.append('g')
+    const axisG = svg.append('g')
       .attr('transform', `translate(0,${marginTop})`)
-      .style('fill', 'white')
+    
+    const axisTransition = (transition: d3.Transition<any, any, any, any>) => {
+      const axis = d3.axisTop(x)
+        .ticks(width / 160)
+        .tickSizeOuter(0)
+        .tickSizeInner(-barSize * (n + y.padding()))
+        .tickFormat(x => formatMillions(+x))
+      
+      axisG.transition(transition)
+        .call(axis)
+      
+      axisG.select(".tick:first-of-type text").remove()
+      axisG.selectAll(".tick:not(:first-of-type) line")
+        .attr("stroke", "white")
+        .attr("stroke-opacity", 0.2)
+      axisG.select(".domain").remove()
+    }
 
     // Implementation of the ticker
     const ticker = svg.append('text')
@@ -135,29 +155,19 @@ export default function BarRace({ data, width = 800, height = 500 }: BarRaceProp
       .attr('y', height - marginBottom - 10)
       .attr('dy', '0.32em')
 
-    // Animation function
+    // Modified animation function
     const animate = async () => {
-      for (const keyframe of keyframes) {
+      while (animationRef.current < keyframes.length) {
+        const keyframe = keyframes[animationRef.current]
         const [date, data] = keyframe
 
         // Update x domain with some padding
         x.domain([0, data[0].value * 1.1])
 
         // Update axis
-        axis.transition()
+        axisTransition(d3.transition()
           .duration(duration)
-          .ease(d3.easeLinear)
-          .call(d3.axisTop(x)
-            .ticks(width / 160)
-            .tickFormat(x => formatMillions(+x))
-          )
-          .call(g => {
-            g.select('.domain').remove()
-            g.selectAll('.tick line')
-              .attr('stroke', 'white')
-            g.selectAll('.tick text')
-              .attr('fill', 'white')
-          })
+          .ease(d3.easeLinear))
 
         // Update bars
         barsG.selectAll<SVGRectElement, DataItem>('rect')
@@ -217,18 +227,74 @@ export default function BarRace({ data, width = 800, height = 500 }: BarRaceProp
         ticker.text(formatDate(date))
 
         await new Promise(resolve => setTimeout(resolve, duration))
+
+        // If last frame, reset
+        if (animationRef.current === keyframes.length - 1) {
+          reset()
+        } else {
+          setCurrentFrame(animationRef.current)
+          if (isPlaying) {
+            animationRef.current++
+          }
+        }
       }
     }
 
+    // Reset function
+    const reset = () => {
+      animationRef.current = 0
+      setCurrentFrame(0)
+      animate()
+    }
+
     // Start animation
-    animate()
+    reset()
 
     // Cleanup
     return () => {
       svg.selectAll('*').remove()
     }
+  }, [data, width, height, isPlaying])
 
-  }, [data, width, height])
+  const handleSliderChange = (value: number) => {
+    animationRef.current = value
+    setCurrentFrame(value)
+  }
 
-  return <svg ref={svgRef} />
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <svg ref={svgRef} />
+      <div className="flex items-center gap-4">
+        <button
+          onClick={() => {
+            animationRef.current = 0
+            setCurrentFrame(0)
+          }}
+          className="h-9 w-9 rounded-full bg-white shadow-md flex items-center justify-center hover:bg-gray-50 active:bg-gray-100 transition-colors"
+          aria-label="Reset"
+        >
+          <ResetIcon className="h-5 w-5 text-gray-700" />
+        </button>
+
+        <div className="relative flex items-center select-none touch-none w-64 h-5">
+          <div className="relative flex-grow h-[6px] rounded-full bg-gray-800/20 border border-white/20">
+            <div 
+              className="absolute h-full rounded-full bg-white"
+              style={{ 
+                width: `${(currentFrame / (data.weeks.length * 7 - 1)) * 100}%`
+              }}
+            />
+          </div>
+          <input 
+            type="range"
+            min={0}
+            max={data.weeks.length * 7 - 1}
+            value={currentFrame}
+            onChange={(e) => handleSliderChange(Number(e.target.value))}
+            className="absolute w-full appearance-none bg-transparent cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-5 [&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:shadow-md [&::-webkit-slider-thumb]:hover:bg-gray-50 [&::-webkit-slider-thumb]:focus:outline-none [&::-webkit-slider-thumb]:focus:ring-4 [&::-webkit-slider-thumb]:focus:ring-gray-800/30 [&::-moz-range-thumb]:w-5 [&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:shadow-md [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:hover:bg-gray-50 [&::-moz-range-thumb]:focus:outline-none [&::-moz-range-thumb]:focus:ring-4 [&::-moz-range-thumb]:focus:ring-gray-800/30"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
